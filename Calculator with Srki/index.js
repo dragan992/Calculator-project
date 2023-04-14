dbc6cc1d1f1ccb18bb1757fd59100a9b2cb1959b
dragan992 <dragan_blok1@hotmail.com>
2023-04-14 21:27:23 +0200

    calculator is finished

--- Calculator with Srki/index.js
+++ Calculator with Srki/index.js
@@ -7,6 +7,8 @@ console.log("Ide gas");
 4.Kad kliknemo matematicke operacije, da nam napise posle prvog broja znak te operacije 
 4a.Ako user pritisne neku matematicku operaciju, a da nije prethodno uneo broj, da se pokaze greska invalid format
 4b.Ako kliknemo jednako posle unete matematicke operacije neke, da se ispise greska, invalid format used
+4c. Ako je samo 0 isto da ispise error
+4d. Ako ne postoje 2 broja ispisi gresku
 5.Kada user klikne prvi broj, pa matematicku operaciju i drugi broj i kada klikne jednako, da mu se ispise tacan rezultat
 5a.Ako user deli prvi broj sa nulom, da mu se ispise greska can't divide by zero 
 */
@@ -18,15 +20,18 @@ console.log("Ide gas");
 // 3. Na C ponisti broj, i da nakon toga bude opet 0 (hint: trebas da implementiras pomocu druge funkcije)
 
 const ekran = document.getElementById("ekran");
-
+const error = document.getElementById('error-message');
+let sign = '';
 function dodajBroj(broj) {
     let prebaciBrojUString = broj.toString();
 
     if (ekran.innerText === "0" && prebaciBrojUString !== "0") {
         ekran.innerText = broj;
+        showHideError(false, '');
     }
     else if (ekran.innerText !== "0") {
         ekran.innerText += broj;
+        showHideError(false, '');
     }
     else return;
 }
@@ -35,7 +40,8 @@ function dodajBroj(broj) {
 const brisanje = document.getElementById("C");
 
 function ocistiEkran() {
-    ekran.innerText = "0"
+    ekran.innerText = "0";
+    showHideError(false, '');
 }
 
 // function brisiPoslednje() {
@@ -50,21 +56,23 @@ function ocistiEkran() {
 
 function brisiPoslednje() {
     ekran.innerText = ekran.innerText.length === 1 ? "0" : ekran.innerText.slice(0, -1);
+    showHideError(false, '');
 }
 
 function znakoviInterpunkcije(znak) {
-    let proba = ekran.innerText.slice(-1);
+    let proba = lastCharacterInString(ekran.innerText);
     if (ekran.innerText === "0") {
-        return;
+        showHideError(true, 'invalid format')
     }
-    else if (proba === znak || isAlreadySign(proba)) {
-        return;
+    else if (proba === znak || isSign(proba)) {
+        showHideError(true, 'invalid format')
     } else {
         ekran.innerText += znak;
+        showHideError(false, '');
     }
 }
 
-function isAlreadySign(sign) {
+function isSign(sign) {
     switch(sign) {
         case '*':
         case '+':
@@ -75,3 +83,79 @@ function isAlreadySign(sign) {
             return false;
     }
 }
+
+function showHideError(isShow, erorMsg) {
+    const display = isShow ? 'display: flex' : 'display: none'; 
+    error.setAttribute('style', display);
+    error.innerText = erorMsg;
+}
+
+function lastCharacterInString(str) {
+    return str.slice(-1);
+}
+
+function jednakost() {
+    
+    const lastChar = lastCharacterInString(ekran.innerText);
+    if(isSign(lastChar)) {
+        showHideError(true, 'invalid format');
+    } else if (ekran.innerText.length === 1 && lastChar === '0') {
+        showHideError(true, 'You need to type some number first');
+    } else if(!areThereTwoNumbers(ekran.innerText)) {
+        showHideError(true, 'One number is missing')
+    } else {
+        calculate(ekran.innerText);
+    }
+
+    
+}
+
+function areThereTwoNumbers(str) {
+    let arr = divideTwoNumbers(str);
+    return arr.length > 1;
+}
+
+function calculate(str) {
+    const arr = divideTwoNumbers(str);
+    let total = 0;
+    if(sign === '*') {
+        total = +arr[0] * +arr[1];
+    } else if(sign === '/') {
+        total = +arr[0] / +arr[1];
+    }
+    else if(sign === '-') {
+        total = +arr[0] - +arr[1];
+    }
+     else if(sign === '+') {
+        total = +arr[0] + +arr[1];
+    }
+    ekran.innerText = total;
+}
+
+function divideTwoNumbers(str) {
+    let indexOfSign = 0;
+    const arr = [];
+    for(let i = 1; i < str.length ; i++) {
+        if(isSign(str[i])) {
+            indexOfSign = i;
+            sign = str[i];
+        } 
+    }
+    
+    let firstNumber = str.substring(0, indexOfSign);
+    console.log(firstNumber);
+    let secondNumber = str.substring(indexOfSign + 1, str.length);
+    if(firstNumber && secondNumber) {
+        arr.push(firstNumber);
+        arr.push(secondNumber);
+    }
+
+    return arr;
+}
+
+
+// FINAL EXAM 
+
+
+// AKO POSTOJE 2 BROJA, NE MOZES VISE DA KORISTIS MATEMATICKE OPERACIJE,
+//  I UKOLIKO USER POSLE DRUGOG BROJA PROBA DA STAVI NEKU OD METAMATICKIH OPERACIJA ISPISI MU GRESKU 'BATO DES POSO'
